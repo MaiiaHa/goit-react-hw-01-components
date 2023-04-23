b748417bfc530e6237b8b6ea5e5f7b491e3bbfa8
MaiiaHa <maiia.robeiko@ukr.net>
2023-04-23 22:34:05 +0200

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,41 @@
-export const App = () => {
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101',
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
+//===============================
+
+import Profile from './profile/Profile';
+import user from '../../src/data/user.json';
+import Statistics from './statistics/Statistics';
+import data from '../../src/data/data.json';
+// import friends from '../../src/data/friends.json';
+// import transaction from '../../src/data/transactions.json';
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        avatarUrl={user.avatar}
+        name={user.username}
+        location={user.location}
+        tag={user.tag}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+      <Statistics title="Upload stats" stats={data} />
     </div>
   );
-};
+}
